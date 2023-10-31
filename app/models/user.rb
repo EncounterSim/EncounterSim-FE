d1a6661617afcefff58befe0d46d83a9f9b510da
Connor Richmond <richmond.connor@gmail.com>
2023-10-31 13:13:53 -0400

    remove username requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
-  validates :username, presence: true
   validates :password, presence: true
 
   has_secure_password
