141c6f939ddf5e84be5c6e08337bb7424b442fb1
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-31 19:56:53 -0500

    update for nav bar and bootstrap styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @_current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def error_message(errors)
+    errors.full_messages.join(', ')
+  end
 end
