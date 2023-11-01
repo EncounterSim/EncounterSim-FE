28a4595028976aaf68dc8f82aefca0d08445b2f4
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-01 17:12:34 -0500

    update facade call

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -1,8 +1,14 @@
 class EncountersController < ApplicationController
   def new
-    @monsters = EncounterSimFacade.new.monster_names
+    @encounter = EncounterSimFacade.new
   end
 
   def create
+    require 'pry';binding.pry
+  end
+
+  private
+  def encounter_params
+    params.permit(:monster, :class_1, :level_1, :ability_modifiers_1, :class_2, :level_2, :ability_modifiers_2, :class_2, :level_2, :ability_modifiers_2, :class_3, :level_3, :ability_modifiers_3, :class_4, :level_4, :ability_modifiers_4, :class_5, :level_5, :ability_modifiers_5, :class_6, :level_6, :ability_modifiers_6)
   end
 end
