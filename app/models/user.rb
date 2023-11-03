e590a4bcab1a61b8d091eb4324909384fa1aefb8
Connor Richmond <richmond.connor@gmail.com>
2023-11-03 13:20:02 -0400

    update user model to allow OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, if: :standard_login?
 
-  has_secure_password
+  has_secure_password validations: false
+
+  def standard_login?
+    password_digest.present? && email.present?
+  end
+
+  validates :uid, uniqueness: true, presence: true, if: :github_login?
+  validates :token, presence: true, if: :github_login?
+  validates :username, presence: true, if: :github_login?
+
+
+  def github_login?
+    uid.present? && token.present? && username.present?
+  end
+
 end
