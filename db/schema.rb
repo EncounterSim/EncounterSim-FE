203a62f223d378156dfa442017e2e8ac7e113f0d
Connor Richmond <richmond.connor@gmail.com>
2023-11-02 20:16:38 -0400

    update table to include needed params

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_02_202621) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "password_digest"
     t.string "email"
     t.datetime "created_at", null: false
