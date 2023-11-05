8dc9b70e1479626c796006bbb50ad857d5578ef9
Robert deLaguna <rjdelaguna@yahoo.com>
2023-11-05 00:37:53 -0700

    add poro to accept results from back end

--- app/services/encounter_sim_service.rb
+++ app/services/encounter_sim_service.rb
@@ -12,17 +12,13 @@ class EncounterSimService
     get_url("/api/v1/players")[:data]
   end
 
-  def monster(monster_index)
-    get_url("/api/v1/monsters/#{monster_index}")
-  end
-
   def encounter_creation(hash_data)
 
-    post = conn.post "api/v1/encounters" do |req|
+    post = conn.post "/api/v1/encounters" do |req|
       req.headers[:content_type] = 'application/json'
       req.body = hash_data.to_json
     end
-    post
+    JSON.parse(post.body, symbolize_names: true)
   end
 
   def get_url(url)
