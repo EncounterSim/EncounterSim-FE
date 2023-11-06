a9e158fa8d173aec9ae4944986fb61efebcc31da
Nick Spencer <strangebrew202@gmail.com>
2023-11-05 18:31:01 -0700

    Add: couple more results stats for encounter results page

--- app/poros/results.rb
+++ app/poros/results.rb
@@ -1,9 +1,12 @@
 class Results
-  attr_reader :sim_id, :total_wins, :total_losses
+  attr_reader :sim_id, :total_wins, :total_losses, :total_rounds,
+              :win_percentage
 
   def initialize(data)
     @sim_id = data[:id]
     @total_wins = data[:total_wins]
     @total_losses = data[:total_loses]
+    @total_rounds = data[:total_rounds]
+    @win_percentage = data[:win_percentage]
   end
 end
