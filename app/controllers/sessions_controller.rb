d1f6bf464acb053d88708f659e30a00ae6b78ac1
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-05 23:41:40 -0600

    sessions controller to handle magic link login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_user_for_magic_link(params) 
+    if user
+      user.update!(login_token: nil, login_token_valid_until: 1.year.ago)
+      session[:user_id] = user.id
+      flash[:Success] = "Congrats, you are signed in!"
+      redirect_to root_path
+    else
+      flash[:error] = "Invalid or expired login link"
+      redirect_to root_path
+    end
+  end
+end
