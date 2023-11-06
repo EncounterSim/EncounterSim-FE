55785be407179eac3759933351a2bfe4cc821b62
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-05 23:46:38 -0600

    routes added for session controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   resources :monsters, only: [:index, :show]
 
   resources :encounters, only: [:new, :create], path: 'create_encounter'
+
+  get 'sessions/create'
 end
