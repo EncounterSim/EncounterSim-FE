4e9d21fdd003299c01e3984b0516b73ae2217b47
Connor Richmond <richmond.connor@gmail.com>
2023-11-07 01:32:52 -0500

    fix: syntax issue

--- config/initializers/github_oauth.rb
+++ config/initializers/github_oauth.rb
@@ -1,5 +1,5 @@
 require 'omniauth'
-require 'omniauth-github
+require 'omniauth-github'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, Rails.application.credentials.github[:client_id], Rails.application.credentials.github[:client_secret], callback_url: 'https://encountersim-fe-ea3ea78fa419.herokuapp.com/auth/github/callback'
