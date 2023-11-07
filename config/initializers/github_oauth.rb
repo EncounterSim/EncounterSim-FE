7831880b075ab21865c1fe3f1136970919a94cea
Connor Richmond <richmond.connor@gmail.com>
2023-11-07 01:28:36 -0500

    update callback link

---
+++ config/initializers/github_oauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :github, Rails.application.credentials.github[:client_id], Rails.application.credentials.github[:client_secret], callback_url: 'https://encountersim-fe-ea3ea78fa419.herokuapp.com/auth/github/callback'
+end
