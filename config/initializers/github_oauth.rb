9b0d1736d3921f47945687cffc26de7fce539a80
Connor Richmond <richmond.connor@gmail.com>
2023-11-07 01:31:37 -0500

    require gem

--- config/initializers/github_oauth.rb
+++ config/initializers/github_oauth.rb
@@ -1,3 +1,6 @@
+require 'omniauth'
+require 'omniauth-github
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, Rails.application.credentials.github[:client_id], Rails.application.credentials.github[:client_secret], callback_url: 'https://encountersim-fe-ea3ea78fa419.herokuapp.com/auth/github/callback'
 end
